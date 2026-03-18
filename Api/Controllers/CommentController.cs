709cacef2900568b5f65faf21cd50875f722e304
MaximoAlejandroKirichuk <MaximoAlejandro.Kirichuk@alumnos.uai.edu.ar>
2026-03-18 15:58:35 -0300

    feat(api): add comment controller endpoints

--- Api/Controllers/CommentController.cs
+++ Api/Controllers/CommentController.cs
@@ -1,6 +1,33 @@
-﻿namespace Api.Controllers;
+﻿using Application.UseCases.Comments.CreateComment;
+using Application.UseCases.Comments.ReplyToComment;
+using Microsoft.AspNetCore.Mvc;
 
-public class CommentController
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/comments")]
+public class CommentController : ControllerBase
 {
+    private readonly ICreateCommentUseCase _createCommentUseCase;
+    private readonly IReplyToCommentUseCase _replyToCommentUseCase;
+
+    public CommentController(ICreateCommentUseCase createCommentUseCase, IReplyToCommentUseCase replyToCommentUseCase)
+    {
+        _createCommentUseCase = createCommentUseCase;
+        _replyToCommentUseCase = replyToCommentUseCase;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateComment([FromBody] CreateCommentRequest request)
+    {
+        var response = await _createCommentUseCase.ExecuteAsync(request);
+        return StatusCode(201,response);
+    }
     
+    [HttpPost]
+    public async Task<IActionResult> ReplyToComment([FromBody] ReplyToCommentRequestDto request)
+    {
+        var response = await _replyToCommentUseCase.ExecuteAsync(request);
+        return StatusCode(201,response);
+    }
 }
